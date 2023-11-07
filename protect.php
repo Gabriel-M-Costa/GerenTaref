479e4c5412780675b8b3999d71cccaba9832fec5
Gabriel-M-Costa <boanergue1@gmail.com>
2023-11-07 19:37:51 -0300

    arquivos obsoletos

--- protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!isset($_SESSION)) {
-  session_start();
-}
-if (!isset($_SESSION["id"])) {
-  die("Você não pode acessar esta página. Faça login<p><br><a href=\"index.php\">Login</a></p>");
-}
-?>
